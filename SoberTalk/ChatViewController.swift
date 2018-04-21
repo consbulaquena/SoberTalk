9dbe503c9d916076a45eb0895f4528562e2dd2ff
Cons Bulaqueña <consbulaquena@gmail.com>
2018-04-21 22:10:01 +0800

    Add Viewcontroller

---
+++ SoberTalk/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatViewController.swift
+//  SoberTalk
+//
+//  Created by Cons Bulaqueña on 21/04/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ SoberTalk/SignInViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SignInViewController.swift
+//  SoberTalk
+//
+//  Created by Cons Bulaqueña on 21/04/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
