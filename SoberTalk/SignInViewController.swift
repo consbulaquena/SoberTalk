534b292922eaf929e0f153ee123e5a5e0d714aea
Cons Bulaqueña <consbulaquena@gmail.com>
2018-04-26 23:06:19 +0800

    Set nav controller as rootVC in Appdelegate

--- SoberTalk/SignInViewController.swift
+++ SoberTalk/SignInViewController.swift
@@ -41,8 +41,7 @@ class SignInViewController: UIViewController {
         
         
         // set nav controller as root
-        
-
+        appDelegate.window?.rootViewController = naviVC
         
     }
     
